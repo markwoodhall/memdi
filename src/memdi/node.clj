d9e7e6430941a3ae0bfbf02d97a8df2d8bc6150a
Mark Woodhall <mark.woodhall@gmail.com>
2016-06-15 21:46:18 +0100

    Experimenting.

--- src/memdi/node.clj
+++ src/memdi/node.clj
@@ -5,33 +5,51 @@
   (write-key [this k v])
   (read-key [this k]))
 
-(defrecord SimpleNode [store name master?])
+(defprotocol MasterNode
+  "A protocol representing a memdi master node."
+  (add-slave [this slave]))
 
-(extend-type SimpleNode Node
+(defrecord InMemoryMasterNode [store name slaves])
+(defrecord InMemorySlaveNode [store name master])
+
+(extend-type InMemoryMasterNode Node
   (write-key
     [this k v]
     (-> (:store this)
         (swap! conj {(keyword k) v}))
-    (:store this))
+    this)
   (read-key
     [this k]
     (-> @(:store this)
         ((keyword k)))))
 
-(defn- node
-  [name master]
-  (SimpleNode. (atom {}) name master))
+(extend-type InMemoryMasterNode MasterNode
+  (add-slave
+    [this slave]
+    (-> (:slaves this)
+        (swap! conj (assoc slave :master this)))
+    (-> (:master slave)
+        (reset! this))
+    this))
+
+(extend-type InMemorySlaveNode Node
+  (write-key
+    [this k v]
+    (-> @(:master this)
+        (write-key k v)))
+  (read-key
+    [this k]
+    (-> @(:store this)
+        ((keyword k)))))
 
 (defn master-node
   "Given a name return a new memdi node with
-  the specified name and `:master? true`"
+  no slaves."
   [name]
-  (node name true))
+   (InMemoryMasterNode. (atom {}) name (atom [])))
 
 (defn slave-node
   "Given a name return a new memdi node with
-  the specified name and `:master? false`"
+  the specified master."
   [name]
-  (node name false))
-
-
+  (InMemorySlaveNode. (atom {}) name (atom {})))
--- test/memdi/node_test.clj
+++ test/memdi/node_test.clj
@@ -1,13 +1,13 @@
 (ns memdi.node-test
   (:require [clojure.test :refer [deftest testing is]]
-            [memdi.node :refer [master-node slave-node Node write-key read-key]]))
+            [memdi.node :refer [master-node slave-node write-key read-key add-slave]]))
 
 (deftest master-node-test
   (testing "A master node should be created with the correct values"
-    (let [expected {:master? true
-                    :name "daredevil"}]
+    (let [expected {:name "daredevil"}]
       (is (.equals expected (-> (master-node "daredevil")
-                                (dissoc :store))))))
+                                (dissoc :store)
+                                (dissoc :slaves))))))
 
   (testing "A master node should accept a write and store the key/value,
            it should be retrievable immediately."
@@ -17,7 +17,17 @@
 
 (deftest slave-node-test
   (testing "A slave node should be created with the correct values"
-    (let [expected {:master? false
-                    :name "daredevil"}]
-      (is (.equals expected (-> (slave-node "daredevil")
-                                (dissoc :store)))))))
+    (let [master (master-node "daredevil")
+          slave (slave-node "foggy")
+          slave (first @(:slaves (add-slave master slave)))
+          expected {:name "foggy"
+                    :master master}]
+      (is (.equals expected (-> slave
+                                (dissoc :store))))))
+
+  (testing "A slave node should accept a write on masters behalf"
+    (let [slave (slave-node "foggy")
+          master (master-node "daredevil")]
+      (add-slave master slave)
+      (write-key slave "key" "String data")
+      (is (= (read-key master "key") "String data")))))
