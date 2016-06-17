f554129c9e13c70c15c3fcdfa71549667aae4b12
Mark Woodhall <mark.woodhall@gmail.com>
2016-06-17 19:59:35 +0100

    Still playing. "Replicate" a key/value, init a new slave.

--- src/memdi/node.clj
+++ src/memdi/node.clj
@@ -9,6 +9,10 @@
   "A protocol representing a memdi master node."
   (add-slave [this slave]))
 
+(defprotocol SlaveNode
+  "A protocol representing a memdi slave node."
+  (replicate-key [this k v]))
+
 (defrecord InMemoryMasterNode [store name slaves])
 (defrecord InMemorySlaveNode [store name master])
 
@@ -17,6 +21,9 @@
     [this k v]
     (-> (:store this)
         (swap! conj {(keyword k) v}))
+    (let [slaves @(:slaves this)]
+        (doseq [slave slaves]
+          (replicate-key slave k v)))
     this)
   (read-key
     [this k]
@@ -30,6 +37,9 @@
         (swap! conj (assoc slave :master this)))
     (-> (:master slave)
         (reset! this))
+    (let [store @(:store this)]
+      (doseq [[k v] store]
+        (replicate-key slave (name k) v)))
     this))
 
 (extend-type InMemorySlaveNode Node
@@ -41,6 +51,12 @@
     [this k]
     (-> @(:store this)
         ((keyword k)))))
+
+(extend-type InMemorySlaveNode SlaveNode
+  (replicate-key
+    [this k v]
+    (-> (:store this)
+        (swap! conj {(keyword k) v}))))
 
 (defn master-node
   "Given a name return a new memdi node with
--- test/memdi/node_test.clj
+++ test/memdi/node_test.clj
@@ -13,7 +13,17 @@
            it should be retrievable immediately."
     (let [node (master-node "daredevil")]
       (write-key node "key" "String data")
-      (is (= (read-key node "key") "String data")))))
+      (is (= (read-key node "key") "String data"))))
+
+  (testing "A master node should accept a write and replicate it to slaves."
+    (let [slave (slave-node "foggy")
+          master (master-node "daredevil")
+          slave2 (slave-node "fisk")]
+      (add-slave master slave)
+      (add-slave master slave2)
+      (write-key master "key" "String data")
+      (is (and (= (read-key slave "key") "String data")
+               (= (read-key slave2 "key") "String data"))))))
 
 (deftest slave-node-test
   (testing "A slave node should be created with the correct values"
@@ -30,4 +40,15 @@
           master (master-node "daredevil")]
       (add-slave master slave)
       (write-key slave "key" "String data")
-      (is (= (read-key master "key") "String data")))))
+      (is (= (read-key master "key") "String data"))))
+
+  (testing "A new slave node should refresh itself"
+    (let [slave (slave-node "foggy")
+          master (master-node "daredevil")
+          slave2 (slave-node "fisk")]
+      (add-slave master slave)
+      (write-key slave "key" "String data")
+      (write-key slave "key2" "String data 2")
+      (add-slave master slave2)
+      (is (and (= (read-key slave2 "key") "String data")
+               (= (read-key slave2 "key2") "String data 2"))))))
